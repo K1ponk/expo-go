02fad9b2953b62e3931238574a187a1e1d602017
ramdanhere <ramdandev04@fuoc.space>
2021-12-10 23:43:21 +0700

    fix

--- templates/contracts/eip20/eip20.sol
+++ templates/contracts/eip20/eip20.sol
@@ -23,7 +23,7 @@ import "./ieip20.sol";
 // implementation of eip20 ./interface.sol
 contract EIP20 is IEIP20 {
 	mapping (address => uint256) private _balances;
-	mapping (address => mapping (address => uint256)) private _alowances;
+	mapping (address => mapping (address => uint256)) private _allowances;
 	uint256 private _totalSupply;
 	string private _name;
 	string private _symbol;
@@ -63,7 +63,7 @@ contract EIP20 is IEIP20 {
 
 	function approve(address _spender, uint256 _value) public virtual override returns (bool) {
 		require(_spender != address(0), "approval to zero address");
-		_alowances[msg.sender][_spender] = _value;
+		_allowances[msg.sender][_spender] = _value;
 
 		emit Approval(msg.sender, _spender, _value);
 
@@ -71,11 +71,11 @@ contract EIP20 is IEIP20 {
 	}
 
 	function allowance(address _owner, address _spender) public view virtual override returns (uint256) {
-		return allowance[_owner][_spender];
+		return _allowances[_owner][_spender];
 	}
 
 	// _safeTransfer for handling all transfer in save way
-	function _safeTransfer(address _from, address _to, uint256 _value) internal pure returns (bool) {
+	function _safeTransfer(address _from, address _to, uint256 _value) internal returns (bool) {
 		require(_from != address(0), "transfer from zero address");
 		require(_to != address(0), "transfer to zero address");
 		require(balanceOf(_from) >= _value, "not enough balance");
@@ -83,7 +83,7 @@ contract EIP20 is IEIP20 {
 		if (_from != msg.sender) {
 			require(allowance(_from, msg.sender) >= _value, "not enough allowances");
 
-			_alowances[_from][msg.sender] -= _value;
+			_allowances[_from][msg.sender] -= _value;
 		}
 
 		_balances[_from] -= _value;
@@ -95,31 +95,27 @@ contract EIP20 is IEIP20 {
 	}
 
 	// token minting function
-	function _mint(address _to, uint256 _value) external returns (bool) {
+	function _mint(address _to, uint256 _value) internal virtual {
 		require(_to != address(0), "mint to zero address");
 		_balances[_to] += _value;
 		_totalSupply += _value;
 
 		emit Transfer(address(0), _to, _value);
-
-		return true;
 	}
 
 	// token burning function
-	function _burn(address _owner, uint256 _value) external view returns (bool) {
+	function _burn(address _owner, uint256 _value) internal virtual {
 		require(_owner != address(0), "burn from zero address");
 		require(balanceOf(_owner) >= _value, "not enough balances");
 		if (_owner != msg.sender) {
 			require(allowance(_owner, msg.sender) >= _value, "not enough allowances");
 
-			_alowances[_owner][msg.sender] -= _value;
+			_allowances[_owner][msg.sender] -= _value;
 		}
 
 		_balances[_owner] -= _value;
 		_totalSupply -= _value;
 
 		emit Transfer(_owner, address(0), _value);
-
-		return true;
 	}
 }
